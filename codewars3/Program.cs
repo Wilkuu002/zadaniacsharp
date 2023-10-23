8cb4a581cfc49669d0610b69415e1637ccab160a
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-10-23 17:20:15 +0200

    23.10.2

---
+++ codewars3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars3
+{
+    public class TwoToOne
+    {
+
+        public static string Longest(string s1, string s2)
+        {
+            string obydwa = s1 + s2;
+            string wynik = new string(obydwa.Distinct().OrderBy(c => c).ToArray());
+
+            return wynik;
+        }
+        public static void Main()
+        {
+            string a = "asdfththhhuyktyjk";
+            string b = "asdfththhbbbgjkuyiouyoqwrewgnhuyktyhhhiooopplqqaszxcjk";
+            Console.WriteLine(Longest(a, b));
+            Console.ReadKey();
+        }
+    }
+}
