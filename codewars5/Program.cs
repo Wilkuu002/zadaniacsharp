0892d81cf041f216e0bd01c14732b21b58011ebd
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-10-24 15:52:53 +0200

    24.10

---
+++ codewars5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars5
+{
+    public class Kata
+    {
+        public static string DuplicateEncode(string word)
+        {
+            Dictionary<char, int> czestoscZnakow = new Dictionary<char, int>();
+            StringBuilder wynik = new StringBuilder();
+
+
+            foreach (char znak in word)
+            { 
+            char malyZnak = char.ToLower(znak);
+                if (czestoscZnakow.ContainsKey(malyZnak))
+                {
+                    czestoscZnakow[malyZnak]++;
+                }
+                else
+                {
+                    czestoscZnakow[malyZnak] = 1;
+                }
+            }
+            foreach (char znak in word)
+            {
+                char malyZnak = char.ToLower(znak);
+                if (czestoscZnakow[malyZnak] > 1)
+                {
+                    wynik.Append(')');
+                }
+                else { wynik.Append('('); }
+            }
+            
+
+            return wynik.ToString();
+        }
+    }
+}
