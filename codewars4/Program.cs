cfea88a850657d9b9ffb1833c168110a1dad8490
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-10-24 14:11:32 +0200

    24.10

---
+++ codewars4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars4
+{
+    public class Kata
+    {
+        public static bool Solution(string str, string ending)
+        {
+            if (str.EndsWith(ending))
+            { return true; }
+            else { return false;
+        }
+    }
+}
