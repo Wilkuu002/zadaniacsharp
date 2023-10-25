13c48f87be06958344fb6bc5dd73326c7e3ef2db
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-10-25 21:07:43 +0200

    25.10

---
+++ codewars8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars8
+{
+    public class Kata
+    {
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            StringBuilder NumerTelefonu = new StringBuilder();
+
+            NumerTelefonu.Append("(");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                NumerTelefonu.Append(numbers[i]);
+
+                if (i == 2)
+                {
+                    NumerTelefonu.Append(") ");
+                }
+                else if (i == 5)
+                {
+                    NumerTelefonu.Append("-");
+                }
+            }
+
+            return NumerTelefonu.ToString();
+        }
+    }
+}
