292930300dc5b4a3e2214287c7db3cb241a0fb8f
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-10-25 13:41:26 +0200

    25.10

---
+++ codewars7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars7
+{
+    public class Accumul
+    {
+        
+        public static String Accum(string s)
+        {
+            StringBuilder wynik = new StringBuilder();
+            char[] tablicacharow = s.ToCharArray();
+            for (int i = 0; i < tablicacharow.Length; i++)
+            {
+                for (int k = 0; k <= i; k++)
+                {
+                    if (k == 0)
+                    {
+                        wynik.Append(char.ToUpper(tablicacharow[i]));
+                    }
+                    else
+                    {
+                        wynik.Append(char.ToLower(tablicacharow[i]));
+                    }
+                }
+                if (i < tablicacharow.Length-1) 
+                {
+                    wynik.Append('-');
+                }
+                
+                    
+            }
+            return wynik.ToString();
+        }
+    }
+}
