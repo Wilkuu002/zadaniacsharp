bcf1862d24e4444c857e1f9b7251c48b3fca03a3
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-10-26 17:51:32 +0200

    26.10

---
+++ codewars9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+
+            int liczba = 0;
+            for (int i = 0; i < seq.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < seq.Length; j++)
+                {
+                    if (seq[i] == seq[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count % 2 != 0)
+                {
+                    liczba = seq[i];
+                }
+            }
+            return liczba;
+        }
+
+    }
+}
+
+
