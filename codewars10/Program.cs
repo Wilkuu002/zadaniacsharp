f8e2b553064b0955849c58abc870d48323ca9c94
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-10-30 15:54:03 +0100

    30.10

---
+++ codewars10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars10
+{
+    public class Kata
+    {
+        public static string SpinWords(string sentence)
+        {
+            StringBuilder zdanie = new StringBuilder();
+            string[] words = sentence.Split(' ');
+            for (int i = 0; i < words.Length; i++) 
+            {
+                if (5 <= words[i].Length)
+                {
+                    string odwroconyWyraz = new string(words[i].Reverse().ToArray());
+                    zdanie.Append(odwroconyWyraz);
+                }
+                else
+                {
+                    zdanie.Append(words[i]);
+                }
+                zdanie.Append(" ");
+             }
+ 
+            return zdanie.ToString().Trim();
+        }
+    }
+}
