b31cc3baaaa882fb243aba589b12671ad8b7fc0a
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-10-31 18:33:32 +0100

    31.10

---
+++ codewars13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars13
+{
+    public class Kata
+    {
+        public static int GetUnique(IEnumerable<int> numbers)
+        {
+            var liczby = new Dictionary<int, int>();
+
+            foreach (int number in numbers)
+            {
+                if (liczby.ContainsKey(number))
+                {
+                    liczby[number]++;
+                }
+                else
+                {
+                    liczby[number] = 1;
+                }
+            }
+
+            foreach (var kvp in liczby)
+            {
+                if (kvp.Value == 1)
+                {
+                    return kvp.Key;
+                }
+            }
+            return 0;
+        }
+    }
+
+}
