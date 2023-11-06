5c9374f4941b543c76cdeca3a3e49d153b8af4f2
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-11-06 20:06:12 +0100

    6.11

---
+++ codewars15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace codewars15
+{
+    public class Kata
+    {
+        public static string Add(string a, string b)
+        {
+            BigInteger liczbaA = BigInteger.Parse(a);
+            BigInteger liczbaB = BigInteger.Parse(b);
+
+            return (liczbaB + liczbaA).ToString();
+        }
+    }
+}
